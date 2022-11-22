b41e2904ca0da60702ae7b01419e934c331d846c
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2022-11-22 23:49:33 +0100

    feat: feature 4

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ console.log('this is a feature');
 console.log('this is a feature2');
 console.log('this is a breaking change');
 console.log('this is a feature3');
+console.log('this is a feature4');
