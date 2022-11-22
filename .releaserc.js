b2139d6b57561fc4b3f109fe542ac86613408a00
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2022-11-22 11:13:44 +0100

    chore: specifying plugins explicitly to disable @semantic-release/npm

--- .releaserc.js
+++ .releaserc.js
@@ -11,5 +11,59 @@ module.exports = {
         }
     ],
     repositoryUrl: "https://github.com/artursudnik/releases-test-1",
-    extends: "@energyweb/semantic-release-config",
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "conventionalcommits",
+                releaseRules: [
+                    { type: "docs", release: "patch" },
+                    { type: "style", release: "patch" },
+                    { type: "refactor", release: "patch" },
+                    { type: "perf", release: "patch" },
+                ],
+                parserOpts: {
+                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+                },
+            },
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                // https://github.com/semantic-release/release-notes-generator#configuration
+                preset: "conventionalcommits",
+                parserOpts: {
+                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+                },
+                // available options are https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.2.0/README.md
+                presetConfig: {
+                    types: [
+                        { type: "feat", section: "Features" },
+                        { type: "fix", section: "Bug Fixes" },
+                        { type: "docs", section: "Documentation" },
+                        { type: "style", section: "Style changes" },
+                        { type: "refactor", section: "Refactoring" },
+                        { type: "perf", section: "Performance improvements" },
+                        { type: "test", hidden: true },
+                        { type: "chore", hidden: true },
+                        { type: "ci", hidden: true },
+                    ],
+                },
+            },
+        ],
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "docs/CHANGELOG.md",
+            },
+        ],
+        // "@semantic-release/npm",
+        "@semantic-release/github",
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json", "docs/CHANGELOG.md"],
+            },
+        ],
+    ]
 };
